76faaff4978fe9c01d889ac988fc02fe684f1841
safecorners <safeconers.mobile@gmail.com>
2021-03-27 08:00:13 +0900

    set up postgres

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/labstack/echo/v4 v4.2.1
 	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.10.0
 )
--- server.go
+++ server.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+
 	"github.com/safecornerscoffee/echo-twitter/handler"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	e := echo.New()
 	e.Logger.SetLevel(log.ERROR)
 	e.Use(middleware.Logger())
+
+	// Database connection
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresPort := os.Getenv("POSTGRES_PORT")
+	postgresUser := os.Getenv("POSTGRES_USER")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	postgresDb := os.Getenv("POSTGRES_DB")
+	pqInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		postgresHost, postgresPort, postgresUser, postgresPassword, postgresDb)
+
+	db, err := sql.Open("postgres", pqInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	for i := 0; i < 360; i++ {
+		if err = db.Ping(); err == nil {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Initialize Handler
 	h := &handler.Handler{}
